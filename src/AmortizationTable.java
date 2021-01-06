7d7688694add639dc0bb2fc80161821d6c78d064
anagha <anagha1996@gmail.com>
2021-01-06 12:40:39 +0530

    Create java project for amortization

---
+++ src/AmortizationTable.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class AmortizationTable {
+
+  class LoanDetails{
+    /**
+     * Class to hold varibles of the 
+     * input from the bank
+     */
+  };
+
+  void calculateAmortizationSchedule() {
+    /**
+     * Logic to calculate table.
+     */
+  };
+
+  void displayAmortizationSchedule() {
+    /**
+     * Logic to display table.
+     */
+  };
+
+  float calculateInterest() {
+    /** 
+     * Logic calculate interest
+     */
+  };
+
+  float calculatePrincipal() {
+    /** 
+     * Logic calculate Principal
+     */
+  };
+
+  public static void main(String[] args) throws Exception {
+
+    /**
+     * Create object and collect input from users
+     * using scanner and call the amortization calculation function
+     */
+
+    System.out.println("Hello, World!");
+  }
+}
