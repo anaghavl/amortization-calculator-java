04566fda969cbd30724de09ade77b9b8a0c72e0a
anagha <anagha1996@gmail.com>
2021-01-06 20:09:45 +0530

    Add setters and getters for the class

--- src/AmortizationTable.java
+++ src/AmortizationTable.java
@@ -5,55 +5,55 @@ import java.util.InputMismatchException;
 public class AmortizationTable {
 
   double calculateMonthlyPayment(LoanDetails details) {
-    double interest = (details.interest *.01)/12;
-    double monthlyPayment = details.loanAmount * (Math.pow((1+interest),details.term)*interest)/((Math.pow((1+interest),details.term))-1);
+
+    double monthlyPayment = details.getLoanAmount() * (Math.pow((1+details.getInterest()),details.getTerm())*details.getInterest())/((Math.pow((1+details.getInterest()),details.getTerm()))-1);
     return monthlyPayment;
   };
 
   void displayAmortizationSchedule(LoanDetails details) {
     double monthlyPayment = calculateMonthlyPayment(details);
-    double monthlyInterestRate = details.interest/12;
-    double interestPaid  = details.loanAmount * (monthlyInterestRate / 100);
+    double interestPaid  = details.getLoanAmount() * details.getInterest();
     double principal = monthlyPayment - interestPaid;
-    double startBalance = details.loanAmount;
+    double startBalance = details.getLoanAmount();
     double endBalance = startBalance - principal;
-    System.out.format("\n%8s %12s %10s %10s %10s %12s\n","Date", "Start Balance", "Principal", "Interest", "Total Pmt", "Balance");
+    System.out.format("\n%10s %12s %10s %10s %10s %12s\n","Date", "Start Balance", "Principal", "Interest", "Total Pmt", "Balance");
 
-    for (int month = 1; month <= details.term; month++) {
+    for (int month = 1; month <= details.getTerm(); month++) {
       System.out.format("%8d %12.2f %10.2f %10.2f %10.2f %12.2f\n", month, startBalance, principal, interestPaid, monthlyPayment, endBalance);
       startBalance = endBalance;
-      endBalance = endBalance - principal;
-      interestPaid  = startBalance * (monthlyInterestRate/100);
+      interestPaid  = startBalance * details.getInterest();
       principal = monthlyPayment - interestPaid;
+      endBalance = endBalance - principal;
     }
   };
 
   public static void main(String[] args) throws Exception {
 
+    AmortizationTable amortizationTable = new AmortizationTable();
     LoanDetails details = new LoanDetails();
-    AmortizationTable aTable = new AmortizationTable();
-
     Scanner input = new Scanner(System.in);
-    boolean bError = true;
+    boolean userInput = true;
     do {
       try {
+        // Read the details from the user
         System.out.println("Enter the Loan Amount: ");
-        details.loanAmount = input.nextDouble();
+        details.setLoanAmount(input.nextDouble());
     
         System.out.println("Enter the term in months: ");
-        details.term = input.nextInt();
+        details.setTerm(input.nextInt());
     
         System.out.println("Enter the Interest Rate: ");
-        details.interest = input.nextDouble();
+        details.setInterest(input.nextDouble() * .01 / 12);
     
         // System.out.println("Enter the requested date in (DD-MM-YYYY):");
         // String date = input.next();
         // SimpleDateFormat dateFormat = new SimpleDateFormat("DD-MM-YYYY");
         // details.requestDate = dateFormat.parse(date);
 
-        aTable.displayAmortizationSchedule(details);
+        // Call the function to display the Amortization Schedule
+        amortizationTable.displayAmortizationSchedule(details);
 
-        bError = false;
+        userInput = false;
 
       } catch (InputMismatchException e) {
           System.out.print("\033[H\033[2J");  
@@ -67,7 +67,7 @@ public class AmortizationTable {
       //     System.out.println( e + "Invalid entry! \nPlease enter the date in the format (DD-MM-YYYY)");
       //     input.next();
       // }
-    } while(bError);
+    } while(userInput);
     input.close();
   }
 }
--- src/LoanDetails.java
+++ src/LoanDetails.java
@@ -1,8 +1,39 @@
 import java.util.Date;
-
 public class LoanDetails {
-  Double interest;
-  Double loanAmount;
-  Integer term;
-  Date requestDate;
+  private Double interest;
+  private Double loanAmount;
+  private Integer term;
+  private Date requestDate;
+
+  public Double getInterest() {
+    return interest;
+  }
+
+  public Double getLoanAmount() {
+    return loanAmount;
+  }
+
+  public Integer getTerm() {
+    return term;
+  }
+
+  public Date getRequestDate() {
+    return requestDate;
+  }
+
+  public void setInterest(Double interestInput) {
+    this.interest = interestInput;
+  }
+
+  public void setLoanAmount(Double loanAmountInput) {
+    this.loanAmount = loanAmountInput;
+  }
+
+  public void setTerm(Integer termInput) {
+    this.term = termInput;
+  }
+
+  public void setTerm(Date requestDateInput) {
+    this.requestDate = requestDateInput;
+  }
 }
